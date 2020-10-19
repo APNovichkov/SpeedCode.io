975fca1bc5182c720a7fda660c1f4539c76bdd86
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-10-19 12:41:21 -0700

    [add] Added more styling to algo detail apge

--- src/pages/algorithmDetail.js
+++ src/pages/algorithmDetail.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
 
 // Import components
 import Stars from "./../components/stars";
 
+// Fake Data
+const name = "Bubble Sort";
+const difficulty = 2;
+const attempts = 1;
+const description = "Iterative Sorting Algorithm";
+const longDescription = "Bubble Sort is an Iterative Sorting Algorithm. It has best case scenario of nLogN time.";
+const bigO = "nlogn";
+const category = "Sorting";
+
 const AlgorithmDetail = (props) => {
-  let name = "Bubble Sort";
-  let difficulty = 2;
-  let attempts = 1;
-  let description = "Iterative Sorting Algorithm";
-  let bigO = "nlogn";
+  
+  let [hasStarted, updateHasStarted] = useState(false);
+
+  let handleStartClick = (event) => {
+    event.preventDefault();
+
+    updateHasStarted(true);
+    console.log("Starting Time");
+  }
+
+
 
   return (
     <div className="algo-detail-wrapper">
@@ -16,7 +32,7 @@ const AlgorithmDetail = (props) => {
         <div className="algo-detail-title-row-wrapper">
           <div className="d-flex justify-content-left">
             <div className="algo-detail-title-wrapper">
-              <h2 className="algo-detail-title">Bubble Sort</h2>
+                <h2 className="algo-detail-title">{name}</h2>
             </div>
             <div className="algo-detail-difficulty-wrapper">
               <Stars numStars={difficulty}></Stars>
@@ -26,19 +42,47 @@ const AlgorithmDetail = (props) => {
         <div className="algo-detail-attributes-row-wrapper">
           <div className="d-flex justify-content-left">
             <div className="algo-detail-big-o-wrapper">
-              <div className="algo-detail-big-o">O(nlogn)</div>
+                <div className="algo-detail-big-o">{bigO}</div>
             </div>
             <div className="algo-detail-category-wrapper">
-              <div className="algo-detail-category">Sorting</div>
+                <div className="algo-detail-category">{category}</div>
             </div>
           </div>
         </div>
         <div className="algo-detail-description-row-wrapper">
           <div className="algo-detail-description-wrapper">
             <p className="algo-detail-description">
-              Bubble Sort is an Iterative Sorting Algorithm. It has best case scenario of nLogN time.
+               {longDescription}
             </p>
           </div>
+        </div>
+      </div>
+      <div className="algo-detail-input-wrapper">
+        <div className="algo-detail-input">
+          <div className="algo-detail-input-header-wrapper">
+            <div className="algo-detail-input-header">
+              <div className="d-flex justify-content-between">
+                <div className="algo-detail-language-type-wrapper">
+                  <div className="algo-detail-language-type">
+                    Python <span className="fas fa-angle-down"></span>
+                  </div>
+                </div>
+                <div className="algo-detail-timer-wrapper">
+                  <div className="algo-detail-timer">00:00</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {!hasStarted ? (
+            <div className="start-button-wrapper" id="start-button">
+                <button onClick={handleStartClick} className="start-button">Start</button>
+            </div>) : (
+                <div className="algo-detail-input-text-wrapper">
+                    <div className="algo-detail-input-text">
+                        <textarea rows="17" className="algo-detail-input-text-ta"></textarea>
+                    </div>
+                </div>
+          )}
         </div>
       </div>
     </div>
