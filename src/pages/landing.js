391d67ee3b5e0fba2972857d3ff54d2577f25b91
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-12-07 23:55:55 -0800

    [add] Added more to the landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -33,50 +33,67 @@ const Landing = (props) => {
           <div className="col-md-3 stats-item">20/20</div>
         </div>
       </div>
-      <div className="landing-description-wrapper">
-        <div className="landing-description-row-wrapper">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="/assets/images/landing1.png" className="landing-image" alt="logo" />
-            </div>
-            <div className="col-md-6">
-              <div className="landing-description-item-wrapper">
-                <div className="landing-description-item-title">
-                  A Variety of Algorithms and
-                  <br />
-                  Data Structures
-                </div>
-                <div className="landing-description-item-text">
-                  Choose from our curated collection of the most popular algorithms and data structures sorted by
-                  category.
+      <div className="landing-description-background-wrapper">
+        <div className="landing-description-wrapper">
+          <div className="landing-description-row-wrapper">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="/assets/images/landing1.png" className="landing-image" alt="logo" />
+              </div>
+              <div className="col-md-6">
+                <div className="landing-description-item-wrapper landing-description-item-wrapper-when-on-right">
+                  <div className="landing-description-item-title">
+                    A Variety of Algorithms and
+                    <br />
+                    Data Structures
+                  </div>
+                  <div className="landing-description-item-text">
+                    Choose from our curated collection of the most popular algorithms and data structures sorted by
+                    category
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="landing-description-row-wrapper">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="landing-description-item-wrapper">
-                <div className="landing-description-item-title">
-                  A Variety of Algorithms and
-                  <br />
-                  Data Structures
+          <div className="landing-description-row-wrapper">
+            <div className="row">
+              <div className="col-md-6">
+                <div className="landing-description-item-wrapper">
+                  <div className="landing-description-item-title">
+                    Learn, Memorize and Perfect
+                    <br />
+                    Writing Data Structures and Algorithms
+                  </div>
+                  <div className="landing-description-item-text">
+                    Choose a language and type over curated implementations of data structures and algorithms in an interactive console
+                  </div>
                 </div>
-                <div className="landing-description-item-text">
-                  Choose from our curated collection of the most popular algorithms and data structures sorted by
-                  category.
+              </div>
+              <div className="col-md-6">
+                <img src="/assets/images/landing2.png" className="landing-image" alt="logo" />
+              </div>
+            </div>
+          </div>
+          <div className="landing-description-row-wrapper">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="/assets/images/landing3.png" className="landing-image" alt="logo" />
+              </div>
+              <div className="col-md-6">
+                <div className="landing-description-item-wrapper landing-description-item-wrapper-when-on-right">
+                  <div className="landing-description-item-title">
+                    Track Statistics and Improve
+                    <br />
+                    Your Skills
+                  </div>
+                  <div className="landing-description-item-text">
+                    Choose from our curated collection of the most popular algorithms and data structures sorted by
+                    category
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-md-6">
-              <img src="/assets/images/landing1.png" className="landing-image" alt="logo" />
-            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-5"></div>
-          <div className="col-md-7"></div>
         </div>
       </div>
     </div>
